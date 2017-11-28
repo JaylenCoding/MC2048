25341c20f3dd354ddf8905ea6461764c0b270079
JunLin Bian <Minecoder@163.com>
2017-11-28 10:48:47 +0800

    功能更新：增加挑战模式(10*10)

--- 2048/Classes/Home/Controller/MCHomeVC.m
+++ 2048/Classes/Home/Controller/MCHomeVC.m
@@ -8,6 +8,8 @@
 
 #import "MCHomeVC.h"
 
+#import "MCClassicGameVC.h"
+
 #if DEBUG
 #define MCLOG(...) NSLog(__VA_ARGS__)
 #else
@@ -26,8 +28,24 @@
 }
 
 - (IBAction)classicButtonTapped:(id)sender {
-    MCLOG(@"classic button tapped");
+    MCClassicGameVC *vc = [MCClassicGameVC gameWithDimension:4
+                                                winThreshold:1024
+                                                   gameTitle:@"2048\n经典模式"
+                                             backgroundColor:[UIColor whiteColor]
+                                               swipeControls:YES];
+    
+    [self presentViewController:vc animated:YES completion:nil];
 }
+
+- (IBAction)extremityButtonTapped:(id)sender {
+    MCClassicGameVC *vc = [MCClassicGameVC gameWithDimension:10
+                                                winThreshold:8192
+                                                   gameTitle:@"2048\n极限模式"
+                                             backgroundColor:[UIColor whiteColor]
+                                               swipeControls:YES];
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
 
 
 @end

d47191ae3bec5035fa240b5bbbb772b1beb9ff5a
JunLin Bian <Minecoder@163.com>
2017-12-01 17:34:50 +0800

    功能增加：自定义导航控制器

---
+++ 2048/Classes/Main/Controller/MCNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MCNavigationController.h
+//  2048
+//
+//  Created by Minecode on 2017/11/28.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCNavigationController : UINavigationController
+
+@end
---
+++ 2048/Classes/Main/Controller/MCNavigationController.m
@@ -0,0 +1,22 @@
+//
+//  MCNavigationController.m
+//  2048
+//
+//  Created by Minecode on 2017/11/28.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+#import "MCNavigationController.h"
+
+@interface MCNavigationController ()
+
+@end
+
+@implementation MCNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
